cf5d914b2654e4629b6a65eea60b9c0e3af8ba4c
aravind4799 <araviku04@gmail.com>
2020-08-23 21:08:54 +0530

    final 0_1 knapsack

--- DP/0_1_knapsack.py
+++ DP/0_1_knapsack.py
@@ -12,7 +12,7 @@
 weight=[5,3,4,2]
 max_weight=5
 value=[60,50,70,30]
-dp_matrix=[[None for j in range(max_weight+1)] for i in range(len(value))]
+dp_matrix=[[None for j in range(max_weight+1)] for i in range(len(value)+1)]
 
 def print_matrix(a):
     for i in range(len(a)):
@@ -22,17 +22,17 @@ def print_matrix(a):
 
 for i in range(len(dp_matrix)):
     for w in range(len(dp_matrix[0])):
-        if i==0 or w==0:
-            print("i==0 or j==0")
+        if w==0 or i==0:
+            print("i==0 or w==0")
             dp_matrix[i][w]=0
             print_matrix(dp_matrix)
-        elif weight[i]<= w:
-            print ("{}<={}".format(value[i],w))
-            dp_matrix[i][w]=max(dp_matrix[i-1][w],dp_matrix[i-1][w-weight[i]]+value[i])
+        elif weight[i-1]<= w:
+            print ("{}<={}".format(weight[i-1],w))
+            dp_matrix[i][w]=max(dp_matrix[i-1][w],dp_matrix[i-1][w-weight[i-1]]+value[i-1])
             print_matrix(dp_matrix)
         else:
             print("else")
             dp_matrix[i][w]=dp_matrix[i-1][w]
             print_matrix(dp_matrix)
 
-print("ans:"+str(dp_matrix[len(value)-1][max_weight]))
+print("ans:"+str(dp_matrix[len(value)][max_weight]))
